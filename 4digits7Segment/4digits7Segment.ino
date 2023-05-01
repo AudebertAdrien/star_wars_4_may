60437d688812e51dcc8698bc17c6d9b1fabb4b81
adrien audebert <audebertadrien.pro@gmail.com>
2023-05-01 18:08:20 +0200

    new version with math

--- 4digits7Segment/4digits7Segment.ino
+++ 4digits7Segment/4digits7Segment.ino
@@ -2,7 +2,6 @@
 
 SevSeg sevseg;
 float timer = 02.00f;
-unsigned int count = 0;
 
 //time_loss && speed_up_time
 int buttonPinA0 = A0;
@@ -35,26 +34,36 @@ void setup() {
   Serial.begin(115200);
 }
 
-void is_less_than_zero(float val) {
-  if (timer - val <= 0)
+void is_valid_decrementation(float val) {
+  if (timer - val <= 0) {
     timer = 0;
-  else if (count == 0 || count >= 60) {
-    Serial.print(timer);
-    Serial.print('\n');
-    timer -= 00.41f + val;
-    Serial.print(timer);
-    Serial.print('\n');
-    count = 0;
-  } else
-    timer -= val;
-}
+  } else {
+    int a = (int)(timer * 100);
+    int b = (int)(val * 100);
 
+    Serial.print((a - b) % 100);
+    Serial.print("\n");
+    Serial.print(a - b);
+    Serial.print("= ");
+    Serial.print(a);
+    Serial.print("-");
+    Serial.print(b);
+    Serial.print("\n");
+
+    if ((a - b) % 100 > 60)
+      timer -= 00.41f;
+
+    //a - b  % 100
+    //(102 - 5) % 100 > 60
+    //timer -= 00.41f;
+    timer -= val;
+  }
+}
 
 void time_loss() {
   buttonState = digitalRead(buttonPinA1);
   if (buttonState == HIGH && A1_pressed == 0) {
-    count += 4;
-    is_less_than_zero(00.05f);
+    is_valid_decrementation(00.05f);
     A1_pressed = 1;
     sevseg.setNumberF(timer, 2);
   }
@@ -75,21 +84,15 @@ void loop() {
 
   if ((int)(timer * 100) > 0 && (currentMillis - previousMillis >= interval)) {
     previousMillis = currentMillis;
-    Serial.print(count);
-    Serial.print('\n');
-
-    if (count == 0 || count >= 60) {
+    if (timer == 0)
       timer -= 00.41f;
-      count = 0;
-    } else
-      timer -= base_time_decrementation;
-    count++;
+    timer -= base_time_decrementation;
     sevseg.setNumberF(timer, 2);
   }
 
   speed_up_time();
   time_loss();
-  
+
   sevseg.refreshDisplay();
 }
 
